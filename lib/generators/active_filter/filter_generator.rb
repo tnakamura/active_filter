a45b7ceb7d20855e3f6531b11396311f9fcc0f00
tnakamura <griefworker@gmail.com>
2012-11-30 22:59:28 +0900

    FilterGenerator でフィールドも指定できるように修正。

--- lib/generators/active_filter/filter_generator.rb
+++ lib/generators/active_filter/filter_generator.rb
@@ -4,8 +4,10 @@ module ActiveFilter
     class FilterGenerator < ::Rails::Generators::NamedBase
       source_root File.expand_path("../templates", __FILE__)
 
+      argument :attributes, :type => :array, :default => [], :banner => "field field"
+
       def create_filter_file
-        template "filter.rb.erb", "app/filters/#{file_name}_filter.rb"
+        template "filter.rb", "app/filters/#{file_name}_filter.rb"
       end
     end
   end
---
+++ lib/generators/active_filter/templates/filter.rb
@@ -0,0 +1,7 @@
+class <%= class_name %>Filter < ActiveFilter::Base
+  model <%= class_name %>
+  <% if 0 < attributes.size %>
+  fields <%= attributes.map{|attr| ":" + attr.name}.join(", ") %>
+  <% end %>
+end
+
