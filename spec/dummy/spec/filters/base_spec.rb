9e78605275f556f007f59f07ec336e8482b76639
tnakamura <griefworker@gmail.com>
2012-12-13 21:38:28 +0900

    ActiveFilter::Base の spec を dummy アプリの下に移動。

--- spec/dummy/spec/filters/base_spec.rb
+++ spec/dummy/spec/filters/base_spec.rb
@@ -1,26 +1,5 @@
 # coding: utf-8
-require_relative "spec_helper"
-
-class TestScope
-  attr_reader :wheres
-
-  def initialize
-    @wheres = []
-  end
-
-  def where(params={})
-    @wheres << params
-    self
-  end
-end
-
-class TestEntry
-  attr_accessor :title, :body
-
-  def self.scoped
-    TestScope.new
-  end
-end
+require File.expand_path("../../spec_helper", __FILE__)
 
 describe "Base" do
   describe "#initialize" do
@@ -49,43 +28,39 @@ describe "Base" do
 
   describe ".model" do
     class TestFilter2 < ::ActiveFilter::Base
-      model TestEntry
+      model Task
     end
 
     it "model に指定したクラスが格納されているべき" do
       @filter = TestFilter2.new
-      @filter.model.should eq(TestEntry)
+      @filter.model.should eq(Task)
     end
   end
 
   describe ".rule" do
     context "ブロックを指定したとき" do
       class TestFilter3 < ::ActiveFilter::Base
-        model TestEntry
-        rule "^foo$" do |scope, value| scope end;
-        rule "^bar$" do |scope, value| scope end;
+        model Task 
+        rule "^name$" do |scope, value| scope.where(:name => value) end;
       end
 
       it "#rules にルールが格納されているべき" do
         @filter = TestFilter3.new
-        @filter.rules.size.should eq(2)
-        @filter.rules[0].pattern.should eq("^foo$")
-        @filter.rules[1].pattern.should eq("^bar$")
+        @filter.rules.size.should eq(1)
+        @filter.rules[0].pattern.should eq("^name$")
       end
     end
 
     context "ラムダ式を指定したとき" do
       class TestFilter4 < ::ActiveFilter::Base
-        model TestEntry
-        rule "^foo$", lambda { |scope, value| scope }
-        rule "^bar$", ->(scope, value){ scope }
+        model Task
+        rule "^name$", lambda { |scope, value| scope.where(:name => value) }
       end
 
       it "#rules にルールが格納されているべき" do
         @filter = TestFilter4.new
-        @filter.rules.size.should eq(2)
-        @filter.rules[0].pattern.should eq("^foo$")
-        @filter.rules[1].pattern.should eq("^bar$")
+        @filter.rules.size.should eq(1)
+        @filter.rules[0].pattern.should eq("^name$")
       end
     end
 
@@ -93,7 +68,7 @@ describe "Base" do
       it "例外が発生するべき" do
         expect {
           class TestFilter10 < ::ActiveFilter::Base
-            model TestEntry
+            model Task
             rule "^foo$"
           end
         }.to raise_error(ArgumentError)
@@ -103,87 +78,93 @@ describe "Base" do
 
   describe ".field" do
     class TestFilter5 < ::ActiveFilter::Base
-      model TestEntry
-      field :title
+      model Task
+      field :name
     end
 
     it "#rules にルールが格納されているべき" do
       @filter = TestFilter5.new
       @filter.rules.size.should eq(1)
-      @filter.rules[0].pattern.should eq("^title$")
+      @filter.rules[0].pattern.should eq("^name$")
     end
   end
 
   describe ".fields" do
     class TestFilter6 < ::ActiveFilter::Base
-      model TestEntry
-      fields :title, :body
+      model Task
+      fields :name, :completed
     end
 
     it "#rules にルールが格納されているべき" do
       @filter = TestFilter6.new
       @filter.rules.size.should eq(2)
-      @filter.rules[0].pattern.should eq("^title$")
-      @filter.rules[1].pattern.should eq("^body$")
+      @filter.rules[0].pattern.should eq("^name$")
+      @filter.rules[1].pattern.should eq("^completed$")
     end
   end
 
   describe "#to_scope" do
     context "マッチするフィルタが１つだけのとき" do
       class TestFilter7 < ::ActiveFilter::Base
-        model TestEntry
-        fields :title, :body
+        model Task
+        field :name
+      end
+
+      before do
+        FactoryGirl.create(:task, :name => "foo")
       end
 
       it "フィルタリング済みのスコープを返すべき" do
-        @filter = TestFilter7.new(:title => "foo", :body => "bar")
+        @filter = TestFilter7.new(:name => "foo", :completed => true)
         scope = @filter.to_scope
-        scope.wheres.count.should eq(2)
-        scope.wheres[0][:title].should eq("foo")
-        scope.wheres[1][:body].should eq("bar")
+        scope.count.should eq(1)
+        scope.first.name.should eq("foo")
       end
     end
 
     context "マッチするフィルタが複数あるとき" do
       class TestFilter11 < ::ActiveFilter::Base
-        model TestEntry
-        rule "^tit", ->(scope, value) { scope.where(:tit => value) }
-        rule "^title$", ->(scope, value) { scope.where(:title => value) }
-        rule "^body$", ->(scope, value) { scope.where(:body => value) }
+        model Task
+        rule "^nam", ->(scope, value) { scope.where(:name => value) }
+        rule "^name$", ->(scope, value) { scope.where(:name => value) }
+      end
+
+      before do
+        FactoryGirl.create(:task, :name => "foo")
       end
 
       it "最初にマッチしたルールでフィルタするスコープを返すべき" do
-        @filter = TestFilter11.new(:title => "foo", :body => "bar")
+        @filter = TestFilter11.new(:name => "foo", :completed => true)
         scope = @filter.to_scope
-        scope.wheres.count.should eq(2)
-        scope.wheres[0][:tit].should eq("foo")
-        scope.wheres[1][:body].should eq("bar")
+        scope.count.should eq(1)
+        scope.first.name.should eq("foo")
       end
     end
   end
 
   describe "#model" do
     class TestFilter8 < ::ActiveFilter::Base
-      model TestEntry
+      model Task
     end
 
     it "指定したモデルの型を取得できるべき" do
       @filter = TestFilter8.new
-      @filter.model.should eq(TestEntry)
+      @filter.model.should eq(Task)
     end
   end
 
   describe "#rules" do
     class TestFilter9 < ::ActiveFilter::Base
-      rule "^foo$" do |scope, value| scope end
-      rule "^bar$" do |scope, value| scope end
+      model Task
+      rule "^name$" do |scope, value| scope end
+      rule "^completed$" do |scope, value| scope end
     end
 
     it "定義したルールを取得できるべき" do
       @filter = TestFilter9.new
       @filter.rules.size.should eq(2)
-      @filter.rules[0].pattern.should eq("^foo$")
-      @filter.rules[1].pattern.should eq("^bar$")
+      @filter.rules[0].pattern.should eq("^name$")
+      @filter.rules[1].pattern.should eq("^completed$")
     end
   end
 end
