d02651411f79f8a0464eb7ba32bb206666a1fd8c
tnakamura <griefworker@gmail.com>
2013-01-23 20:43:43 +0900

    fixed #16 ジェネレーターで生成するフィルターに order を追加。

--- lib/generators/active_filter/templates/filter.rb
+++ lib/generators/active_filter/templates/filter.rb
@@ -1,7 +1,8 @@
 class <%= class_name %>Filter < ActiveFilter::Base
   model <%= class_name %>
   <% if 0 < attributes.size %>
-  fields <%= attributes.map{|attr| ":" + attr.name}.join(", ") %>
+  fields <%= attributes.map{ |attr| ":" + attr.name }.join(", ") %>
+  #order <%= attributes.map{ |attr| "'#{attr.name} ASC'" }.join(", ") %>
   <% end %>
 end
 
--- spec/dummy/app/filters/task_filter.rb
+++ spec/dummy/app/filters/task_filter.rb
@@ -2,6 +2,7 @@ class TaskFilter < ActiveFilter::Base
   model Task
   
   fields :name, :description, :completed, :deadline_at
+  #order 'name ASC', 'description ASC', 'completed ASC', 'deadline_at ASC'
   
 end
 
