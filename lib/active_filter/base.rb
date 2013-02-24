6de7cfbd3455ee98c77d1f6bf352c24abc1a2cca
tnakamura <griefworker@gmail.com>
2013-02-24 14:35:42 +0900

    fixed #22 _プレフィックスがついたメソッドをプライベートにした。

--- lib/active_filter/base.rb
+++ lib/active_filter/base.rb
@@ -11,19 +11,19 @@ module ActiveFilter
       @data = data
       @scope = scope
 
-      columns = self.class._model.columns
-      
+      columns = self.model.columns
+
       # fields で指定した列だけをフィルタ可能にする
-      fields = self.class._fields
+      fields = self.class.__send__(:_fields)
       unless fields.empty?
         columns = columns.select { |column|
           fields.include?(column.name) ||
             fields.include?(column.name.to_sym)
         }
       end
-      
+
       # exclude で指定した列を除外する
-      excludes = self.class._exclude
+      excludes = self.class.__send__(:_exclude)
       unless excludes.empty?
         columns = columns.reject { |column|
           fields.include?(column.name) ||
@@ -36,20 +36,119 @@ module ActiveFilter
       }
     end
 
-    def self._fields #:nodoc:
-      @fields ||= []
+    module ClassMethods
+      # フィルタを作成する対象のモデルを指定します。
+      def model(klass)
+        # Class クラスのインスタンスである ActiveFilter::Base オブジェクトの
+        # インスタンス変数にモデルの型を格納
+        unless klass.ancestors.include?(ActiveRecord::Base)
+          raise ArgumentError.new("klass required inherit ActiveRecord::Base")
+        end
+        @model = klass
+      end
+
+      def fields(*names)
+        # Class クラスのインスタンスである ActiveFilter::Base オブジェクトの
+        # インスタンス変数にフィールド名を格納
+        @fields = names
+      end
+
+      def exclude(*names)
+        @excludes = names
+      end
+
+      def order(*names)
+        @orders = names
+      end
+
+      private
+      def _model
+        @model
+      end
+
+      def _fields
+        @fields ||= []
+      end
+
+      def _exclude
+        @excludes ||= []
+      end
+
+      def _orders
+        @orders ||= []
+      end
+    end
+    extend ClassMethods
+
+    def model
+      self.class.__send__(:_model)
     end
 
-    def self._model #:nodoc:
-      @model
+    def each(&block)
+      scope = self.to_scope
+      unless block_given?
+        return scope.each
+      end
+
+      scope.each do |obj|
+        block.call(obj)
+      end
+      self
     end
 
-    def self._exclude #:nodoc:
-      @excludes ||= []
+    def to_scope
+      scope = _scoped
+      matched = false
+
+      @filters.each do |filter|
+        lookups = _lookups_from_filter(filter)
+        lookups.each do |lookup, lookup_type|
+          if @data.include?(lookup)
+            converted_value = filter.convert_value(@data[lookup])
+            scope = filter.filter(scope, converted_value, lookup_type)
+            matched = true
+          elsif @data.include?(lookup.to_sym)
+            converted_value = filter.convert_value(@data[lookup.to_sym])
+            scope = filter.filter(scope, converted_value, lookup_type)
+            matched = true
+          end
+        end
+      end
+
+      # ユーザーの入力がフィルタにかからなかった場合は
+      # 該当無しにする
+      unless matched
+        scope = scope.where("1 = 2")
+      end
+
+      self.class.__send__(:_orders).each do |order|
+        scope = scope.order(order.to_s)
+      end
+
+      scope
     end
 
-    def self._orders #:nodoc:
-      @orders ||= []
+    private
+    # コンストラクタで受け取ったスコープまたは
+    # model.scoped を返す
+    def _scoped
+      if @scope.nil?
+        self.model.scoped
+      else
+        @scope
+      end
+    end
+
+    def _lookups_from_filter(filter)
+      lookups = { filter.name => "exact" }
+
+      lookup_types = [filter.lookup_type].flatten
+      lookup_types.each do |lookup_type|
+        lookup = "#{filter.name}__#{lookup_type}"
+        lookups[lookup] = lookup_type
+      end
+
+      lookups
     end
 
     def _create_filter_from_column(column)
@@ -81,103 +180,6 @@ module ActiveFilter
       else
         raise ArgumentError.new("#{column.type} is not supported.")
       end
-    end
-    private :_create_filter_from_column
-
-    # コンストラクタで受け取ったスコープまたは
-    # model.scoped を返す
-    def _scoped
-      if @scope.nil?
-        self.class._model.scoped
-      else
-        @scope
-      end
-    end
-    private :_scoped
-
-    # フィルタを作成する対象のモデルを指定します。
-    def self.model(klass)
-      # Class クラスのインスタンスである ActiveFilter::Base オブジェクトの
-      # インスタンス変数にモデルの型を格納
-      unless klass.ancestors.include?(ActiveRecord::Base)
-        raise ArgumentError.new("klass required inherit ActiveRecord::Base")
-      end
-      @model = klass
-    end
-
-    def self.fields(*names)
-      # Class クラスのインスタンスである ActiveFilter::Base オブジェクトの
-      # インスタンス変数にフィールド名を格納
-      @fields = names
-    end
-
-    def self.exclude(*names)
-      @excludes = names
-    end
-
-    def self.order(*names)
-      @orders = names
-    end
-
-    def model
-      self.class._model
-    end
-
-    def each(&block)
-      scope = self.to_scope
-      unless block_given?
-        return scope.each
-      end
-
-      scope.each do |obj|
-        block.call(obj)
-      end
-      self
-    end
-
-    def _lookups_from_filter(filter)
-      lookups = { filter.name => "exact" }
-
-      lookup_types = [filter.lookup_type].flatten
-      lookup_types.each do |lookup_type|
-        lookup = "#{filter.name}__#{lookup_type}"
-        lookups[lookup] = lookup_type
-      end
-
-      lookups
-    end
-    private :_lookups_from_filter
-
-    def to_scope
-      scope = _scoped
-      matched = false
-
-      @filters.each do |filter|
-        lookups = _lookups_from_filter(filter)
-        lookups.each do |lookup, lookup_type|
-          if @data.include?(lookup)
-            converted_value = filter.convert_value(@data[lookup])
-            scope = filter.filter(scope, converted_value, lookup_type)
-            matched = true
-          elsif @data.include?(lookup.to_sym)
-            converted_value = filter.convert_value(@data[lookup.to_sym])
-            scope = filter.filter(scope, converted_value, lookup_type)
-            matched = true
-          end
-        end
-      end
-
-      # ユーザーの入力がフィルタにかからなかった場合は
-      # 該当無しにする
-      unless matched
-        scope = scope.where("1 = 2")
-      end
-
-      self.class._orders.each do |order|
-        scope = scope.order(order.to_s)
-      end
-
-      scope
     end
   end
 end
